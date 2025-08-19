4f3d2a036862b20b5b90429ce99b172751a1e5b8
allesonsales <allesonsales@gmail.com>
2025-08-19 13:15:54 -0300

    Alterando environment

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const authRoutes = require("./Routes/RoutesAuth");
 const pagamentosRoutes = require("./Routes/RoutesPagamentos");
 const port = process.env.APP_PORT;
 
-app.use(cors({ origin: "http://localhost:4200", credentials: true }));
+app.use(cors({ origin: "https://allesonsales.github.io", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -34,4 +34,4 @@ try {
   console.log(error);
 }
 
-// https://allesonsales.github.io
+// https://allesonsales.github.io http://localhost:4200
